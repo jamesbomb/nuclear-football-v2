33e6ff24a22758dbcdb6ffb06839bdc60c0cfbd7
bomba <angryzapper@gmail.com>
2024-06-30 19:08:59 +0200

    testing conf, add START SCRIPT AND SYS SERVICE CONF

--- start-electron.sh
+++ start-electron.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export DISPLAY=:0
-export XAUTHORITY=/root/.Xauthority
+export XAUTHORITY=$HOME/.Xauthority
 sleep 1
 #/usr/bin/env electron /var/nuclear-football-v2 
 cd /var/nuclear-football-v2
-#npm start --no-sandbox
-npx electron . --no-sandbox --trace-warnings > /var/nuclear-football-v2/electron.log 2>&1
+npm start
+#npx electron . --no-sandbox --trace-warnings > /var/nuclear-football-v2/electron.log 2>&1
